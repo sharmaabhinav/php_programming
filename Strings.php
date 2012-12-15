a4681a51adacf259f5ee740238703daf32919ad2
abhinav <abhinav@abhinav-pc>
2012-12-15 13:45:30 +0530

    Strings in php extend

--- Strings.php
+++ Strings.php
@@ -1,6 +1,7 @@
 <?php
 	# varaiables can be used inside double quoted strings
 	# escape character \n \t are supported in double quoted strings
+	
 	$name = 'abhinav';
 	$string1 = 'my name is $name';		
 	
@@ -8,4 +9,19 @@
 	echo $string1;
 	echo "\n";
 	echo $string2;
+
+	# equality test #
+	$str1 = 'mystring';
+	$str2 = "mystring";
+	if($str1 == $str2){
+		echo "Strings equal";
+	}
+	
+	# string check #
+	if(is_string($str1)){
+		echo "string";
+	}	 
+
+	
+	
 ?>
