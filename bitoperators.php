f7352a0f383ea137bf65b9c039a9fd281ff13fdd
abhinav <abhinav@abhinav-pc>
2012-12-22 23:03:16 +0530

    bitwise and logical operators

---
+++ bitoperators.php
@@ -0,0 +1,16 @@
+<?php
+	//bitwise operators & | ^ << >>
+	// logical operators &&,and,||,or,!,negation,xor short circuit 
+
+	$string1 = 'wolf';
+	$string2 = 'cat';
+	$num1 = 5;
+	$string3 = $string1 & $string2;
+	//echo $string3;
+	echo $num1 << 1; // left shift  by one bit not modifies original no
+	echo $num1;
+	echo $num1 >> 1; //101(5) --> 10 (2) right shift by one bit not modifies original no
+
+
+
+?>
